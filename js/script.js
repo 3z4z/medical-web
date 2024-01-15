8fd1c24139d7e446c74546773342939ef87dd59d
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2024-01-15 16:15:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(function(){
+  var textMapping = {
+    'review1': 'I recently had to bring my child to ProHealth for a minor injury, and I was so impressed with the care he received. The pediatrician was great with him and made him feel at ease, and the entire staff was kind and attentive.I recently had to bring my child to ProHealth for a minor injury.',
+    'review2': ' I was so impressed with the care he received. The pediatrician was great with him and made him feel at ease, and the entire staff was kind and attentive.',
+    'review3': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur explicabo temporibus culpa adipisci numquam ut hic optio voluptate iste, id eligendi fuga, modi fugit aut pariatur veritatis maiores ratione aliquam!'
+  };
+  var rateMapping = {
+    'review1': "<i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i>",
+    'review2': "<i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i>",
+    'review3': "<i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i><i class='ri-star-fill'></i>",
+  }
+    $('.preloader').preloadinator({
+        minTime: 2000,
+    });
+    var offDays = [5]; // 0 is Sunday, 1 is Monday, ..., 6 is Saturday
+    $("#datepicker").datepicker({
+      beforeShowDay: function(date) {
+        var day = date.getDay();
+        // Highlight off days
+        return [offDays.indexOf(day) === -1, ''];
+      }
+    });
+    $('.department-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        nextArrow: '.slider-next',
+        prevArrow: '.slider-prev',
+        responsive: [
+            {
+              breakpoint: 1199,
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
+    });
+    $('.reviewer-card').on('click', function(){
+      
+      var toggleReview = $(this).data('target');
+      $('.reviewer-dot').removeClass('scale-up');
+      $('#'+ toggleReview).toggleClass('scale-up');
+
+      var reviewText = textMapping[toggleReview];
+      $('.review-body-text').fadeOut(100, function(){
+        $(this).text(reviewText).fadeIn(100);
+      });
+      var reviewRate = rateMapping[toggleReview];
+      $('.review-body-rating').fadeOut(100, function(){
+        $(this).html(reviewRate).fadeIn(100);
+      });
+    })
+    $('.review-body-text').text(textMapping['review1']).fadeIn(100);
+    $('.review-body-rating').html(rateMapping['review1']).fadeIn(100);
+
+    $('#timepicker').on('click', function(){
+      console.log('click');
+      $('.timelist').toggleClass('toggle-timelist');
+    });
+    $(".timelist li").on('click', function () {
+      var selectedValue = $(this).data("value");
+      $("#timepicker").val(selectedValue);
+      $(".timelist").removeClass('toggle-timelist');
+    });
+});
+$(document).on('click', (event) => {
+  if (!$(event.target).closest('#timepicker').length) {
+    $('.timelist').removeClass('toggle-timelist');
+  }
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+Process to run this project on **VS Code**
+
+1) open this folder to **VS Code**
+2) go to terminal and type `npm install`
+3) download [live server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) & [live sass compiler](https://marketplace.visualstudio.com/items?itemName=ritwickdey.live-sass) extension for vs codes if you don't have installed
+4) you will see two new button <i>**watch sass**</i> & <i>**go live**</i> at the bottom right corner of **VS Code**.
+5) press <i>**watch sass**</i> at first and then <i>**go live**</i>.
+6) you are all set
